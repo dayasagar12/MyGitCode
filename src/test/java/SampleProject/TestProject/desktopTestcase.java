57d5cb749a0bc0b650f1cd3aa34d0b5c82ac1ffd
dayasagar12 <skdaya@gmail.com>
2019-05-14 23:32:03 +0530

    Architect 1 code changes

---
+++ src/test/java/SampleProject/TestProject/desktopTestcase.java
@@ -0,0 +1,25 @@
+package SampleProject.TestProject;
+
+import org.testng.annotations.Test;
+
+public class desktopTestcase {
+	
+	
+	@Test
+	public void desktoptescase1() {
+	
+		
+		System.out.println("This is a UI test case 1");
+		System.out.println("This is a UI test case 1.1");
+		
+	}
+	
+	@Test
+	public void desktoptescase2() {
+	
+		
+		System.out.println("This is a UI test case 2");
+		System.out.println("This is a UI test case 2.1");
+	}
+
+}

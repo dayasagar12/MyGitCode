9ff0ec9193dc035790d92cd8a8b23bfb6cc339e0
dayasagar12 <skdaya@gmail.com>
2019-05-14 23:54:38 +0530

    Final Commit

---
+++ src/test/java/SampleProject/TestProject/newTest.java
@@ -0,0 +1,19 @@
+package SampleProject.TestProject;
+
+import org.testng.annotations.Test;
+
+public class newTest {
+
+	
+	
+
+	@Test
+	public void newTest1() {
+	
+		
+		System.out.println("This is a new test 1");
+		System.out.println("This is a new test 1.1");
+		
+	}
+	
+}
